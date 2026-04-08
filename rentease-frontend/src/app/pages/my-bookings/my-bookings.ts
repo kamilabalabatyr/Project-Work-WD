9aa70f10fcba12ff81df7c71596be1a563b452d8
kamilabalabatyr <k_balabatyr@kbtu.kz>
2026-04-09 01:16:14 +0500

    frontend done

--- rentease-frontend/src/app/pages/my-bookings/my-bookings.ts
+++ rentease-frontend/src/app/pages/my-bookings/my-bookings.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { BookingService } from '../../services/booking.service';
+import { IBooking } from '../../interfaces/booking.interface';
 
 @Component({
   selector: 'app-my-bookings',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './my-bookings.html',
-  styleUrl: './my-bookings.scss',
+  styleUrl: './my-bookings.scss'
 })
-export class MyBookings {
+export class MyBookings implements OnInit {
+  bookings: IBooking[] = [];
+  isLoading = true;
+  errorMsg = '';
 
+  constructor(private bookingService: BookingService) {}
+
+  ngOnInit(): void {
+    this.bookingService.getMyBookings().subscribe({
+      next: (data) => {
+        this.bookings = data;
+        this.isLoading = false;
+      },
+      error: () => {
+        this.errorMsg = 'Не удалось загрузить бронирования';
+        this.isLoading = false;
+      }
+    });
+  }
 }
